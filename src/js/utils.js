7cb3e1302542718e6417d5686ab011a8e752697e
Mikita <dixtry3826@gmail.com>
2025-03-10 23:24:49 +0100

    Initial commit

---
+++ src/js/utils.js
@@ -0,0 +1,57 @@
+/**
+ * @todo
+ * @param index - индекс поля
+ * @param boardSize - размер квадратного поля (в длину или ширину)
+ * @returns строка - тип ячейки на поле:
+ *
+ * top-left
+ * top-right
+ * top
+ * bottom-left
+ * bottom-right
+ * bottom
+ * right
+ * left
+ * center
+ *
+ * @example
+ * ```js
+ * calcTileType(0, 8); // 'top-left'
+ * calcTileType(1, 8); // 'top'
+ * calcTileType(63, 8); // 'bottom-right'
+ * calcTileType(7, 7); // 'left'
+ * ```
+ * */
+export function calcTileType(index, boardSize) {
+  switch (index) {
+    case 0:
+      return "top-left";
+    case boardSize - 1:
+      return "top-right";
+    case boardSize ** 2 - 1:
+      return "bottom-right";
+    case boardSize ** 2 - boardSize:
+      return "bottom-left";
+  }
+  if (index < boardSize - 1) return "top";
+
+  if (boardSize * (boardSize - 1) < index) return "bottom";
+
+  if (index % boardSize == 0) return "left";
+
+  if (index % boardSize == boardSize - 1) return "right";
+
+  return "center";
+}
+
+export function calcHealthLevel(health) {
+  if (health < 15) {
+    return "critical";
+  }
+
+  if (health < 50) {
+    return "normal";
+  }
+
+  return "high";
+}
